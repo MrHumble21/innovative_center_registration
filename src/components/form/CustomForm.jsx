d3f52667476bf64e25d3328102dc762979586dcc
MunisaR <munisrahmatullayeva04@gmail.com>
2023-03-04 17:20:46 +0500

    feat: added components

--- src/components/form/CustomForm.jsx
+++ src/components/form/CustomForm.jsx
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import Lottie from "react-lottie";
 import { regions } from "../../constants/regions";
 import form_animation from "./form_animation.json";
-import image_upload from "./image_upload.json";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { BiImageAdd } from "react-icons/bi";
 import { app_colors } from "../../utils/colors";
-import Input from "./CustomInput";
 import CustomInput from "./CustomInput";
+import CustomRadioButton from "./CustomRadioButton";
 const defaultOptions = {
   loop: true,
   autoplay: true,
@@ -19,7 +18,40 @@ const defaultOptions = {
 };
 
 function CustomForm() {
-  const [value, setValue] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [passport, setPassport] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState("");
+  const [phone, setPhone] = useState("");
+  const [image, setImage] = useState("");
+  const [gender, setGender] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      passport: passport,
+      dateOfBirth: dateOfBirth,
+      phone: phone,
+      image: image,
+      gender: gender,
+    };
+    console.log(data);
+
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassport("");
+    setDateOfBirth("");
+    setPhone("");
+    setImage("");
+
+    setGender();
+  };
   return (
     <>
       <div
@@ -38,91 +70,81 @@ function CustomForm() {
                 <h2
                   style={{
                     fontWeight: "bolder",
-                    // textAlign: "right",
                   }}
                 >
-                  Register with us
+                  Register for an exam
                 </h2>
+                <CustomInput
+                  type={"text"}
+                  label={"First Name (per PASSPORT)"}
+                  value={firstName}
+                  htmlfor={"exampleInputEmail1"}
+                  aria_describedly={"emailHelp"}
+                  labelClass={"form-label mt-2"}
+                  className={"form-control"}
+                  id={"exampleInputEmail1"}
+                  handleChange={(e) => {
+                    console.log(firstName);
+                    setFirstName(e.target.value);
+                  }}
+                />
+                <CustomInput
+                  type={"text"}
+                  label={"Last Name (per PASSPORT)"}
+                  value={lastName}
+                  htmlfor={"exampleInputEmail1"}
+                  aria_describedly={"emailHelp"}
+                  labelClass={"form-label mt-2"}
+                  className={"form-control"}
+                  id={"exampleInputEmail1"}
+                  handleChange={(e) => {
+                    console.log(lastName);
+                    setLastName(e.target.value);
+                  }}
+                />
+                <CustomRadioButton
+                  label={"Gender"}
+                  handleChange={(e) => {
+                    console.log(gender);
+                    setGender(e.target.value);
+                  }}
+                />
+
+                <CustomInput
+                  type={"text"}
+                  label={"Passport or ID Number (e.g. AA1234567)"}
+                  value={passport}
+                  htmlfor={"exampleInputEmail1"}
+                  aria_describedly={"emailHelp"}
+                  labelClass={"form-label mt-2"}
+                  className={"form-control"}
+                  id={"exampleInputEmail1"}
+                  handleChange={(e) => {
+                    console.log(passport);
+                    setPassport(e.target.value);
+                  }}
+                />
+
+                <CustomInput
+                  type={"date"}
+                  label={"Date of birth"}
+                  value={dateOfBirth}
+                  htmlfor={"exampleInputEmail1"}
+                  aria_describedly={"emailHelp"}
+                  labelClass={"form-label mt-2"}
+                  className={"form-control"}
+                  id={"exampleInputEmail1"}
+                  handleChange={(e) => {
+                    console.log(dateOfBirth);
+                    setDateOfBirth(e.target.value);
+                  }}
+                />
+
                 <div className="form-group">
-                  <label for="exampleInputEmail1" className="form-label mt-2 ">
-                    First Name (per PASSPORT)
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <CustomInput type={"text"} />
-                <div className="form-group">
-                  <label for="exampleInputEmail1" className="form-label mt-2 ">
-                    Last Name (per PASSPORT)
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <fieldset class="form-group d-flex flex-column align-items-start">
-                  <h5 class="mt-2 ">Gender</h5>
-                  <div className=" d-flex">
-                    <div class="form-check mx-2 ">
-                      <input
-                        class="form-check-input"
-                        type="radio"
-                        name="optionsRadios"
-                        id="optionsRadios1"
-                        value="option1"
-                        checked=""
-                      />
-                      <label class="form-check-label" for="optionsRadios1">
-                        male
-                      </label>
-                    </div>
-                    <div class="form-check">
-                      <input
-                        class="form-check-input"
-                        type="radio"
-                        name="optionsRadios"
-                        id="optionsRadios2"
-                        value="option2"
-                      />
-                      <label class="form-check-label" for="optionsRadios2">
-                        female
-                      </label>
-                    </div>
-                  </div>
-                </fieldset>
-                <div className="form-group">
-                  <label for="exampleInputEmail1" className="form-label mt-2 ">
-                    Passport or ID Number (e.g. AA1234567)
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div className="form-group">
-                  <label for="exampleInputEmail1" className="form-label mt-2 ">
-                    Date of birth
-                  </label>
-                  <input
-                    type="date"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="exampleSelect1" class="form-label mt-2 ">
+                  <label htmlFor="exampleSelect1" className="form-label mt-2 ">
                     Select Region
                   </label>
-                  <select class="form-select" id="exampleSelect1">
+                  <select className="form-select" id="exampleSelect1">
                     <option></option>
 
                     {regions.map((region, i) => (
@@ -133,49 +155,61 @@ function CustomForm() {
                   </select>
                 </div>
                 <div className="form-group">
-                  <label for="exampleSelect1" class="form-label mt-2 ">
+                  <label htmlFor="exampleSelect1" className="form-label mt-2 ">
                     Phone Number
                   </label>
                   <PhoneInput
                     className="form-control"
                     def
                     placeholder="Enter phone number"
-                    value={value}
-                    onChange={setValue}
+                    value={phone}
+                    onChange={setPhone}
                   />
                 </div>
-                <div className="form-group">
-                  <label for="exampleInputEmail1" className="form-label mt-2 ">
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                  />
-                </div>
-                <div className="form-group">
-                  <h6 className="mt-2 ">Attach passport</h6>
-                  <label htmlFor="image_uplo ad">
-                    <BiImageAdd size={80} color={app_colors.violet} />
-                  </label>
-                  <input
-                    type="file"
-                    className="visually-hidden"
-                    id="image_upload"
-                  />
-                </div>
+                <CustomInput
+                  type={"email"}
+                  label={"Email"}
+                  value={email}
+                  htmlfor={"exampleInputEmail1"}
+                  aria_describedly={"emailHelp"}
+                  labelClass={"form-label mt-2"}
+                  className={"form-control"}
+                  id={"exampleInputEmail1"}
+                  handleChange={(e) => {
+                    console.log(email);
+                    setEmail(e.target.value);
+                  }}
+                />
+                <h6 className="mt-2 ">Attach passport</h6>
+                <CustomInput
+                  htmlfor={"image_upload"}
+                  type={"file"}
+                  className={"visually-hidden"}
+                  id={"image_upload"}
+                  value={image}
+                  label={
+                    <BiImageAdd
+                      size={80}
+                      color={app_colors.violet}
+                      handleChange={(e) => {
+                        setImage(e.target.value);
+                      }}
+                    />
+                  }
+                />
                 <div className="container">
-                  <fieldset class="form-group">
-                    <div class="form-check">
+                  <fieldset className="form-group">
+                    <div className="form-check">
                       <input
-                        class="form-check-input"
+                        className="form-check-input"
                         type="checkbox"
                         value=""
                         id="flexCheckDefault"
                       />
-                      <label class="form-check-label" for="flexCheckDefault">
+                      <label
+                        className="form-check-label"
+                        htmlFor="flexCheckDefault"
+                      >
                         I have read and agreed on <b>Term and Contions</b>
                       </label>
                     </div>
@@ -184,13 +218,14 @@ function CustomForm() {
                 <div className=" d-flex justify-content-start mt-2">
                   <button
                     type="button"
-                    className="custom-btn text-white "
+                    className="custom-btn text-white my-2 "
                     style={{
                       backgroundColor: app_colors.violet,
                       borderRadius: "10px",
                     }}
+                    onClick={handleSubmit}
                   >
-                    Primary
+                    Register
                   </button>
                 </div>
               </div>
--- src/components/form/CustomInput.jsx
+++ src/components/form/CustomInput.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
-const CustomInput = ({ label, type, value, handleChange }) => {
+const CustomInput = ({
+  label,
+  type,
+  value,
+  handleChange,
+  className,
+  id,
+  aria_describedly,
+  htmlfor,
+  labelClass,
+}) => {
   return (
     <div className="form-group">
-      <label for="exampleInputEmail1" className="form-label mt-2 ">
+      <label htmlFor={type === "file" ? id : null} className={labelClass}>
         {label}
       </label>
       <input
         type={type}
         value={value}
-        className="form-control"
-        id="exampleInputEmail1"
-        aria-describedby="emailHelp"
+        className={className}
+        id={type === "file" ? id : null}
+        aria-describedby={aria_describedly}
         onChange={handleChange}
       />
     </div>
